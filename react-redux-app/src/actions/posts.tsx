35e8c999f6df5e08461077e9d661474141d80b02
heehminh <mink141416@gmail.com>
2023-03-25 21:41:08 +0900

    [REFACTOR] modern ES2015

--- react-redux-app/src/actions/posts.tsx
+++ react-redux-app/src/actions/posts.tsx
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-export const fetchPosts = (): any => {
-  return async function fetchPostsThunk(dispatch: any, getState: any) {
-    // action을 보내거나, state를 읽는 로직 작성
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    dispatch({ type: "FETCH_POSTS", payload: response.data });
-  };
+/** modern ES2015 형태로 작성 */
+export const fetchPosts = (): any => async (dispatch: any, getState: any) => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
