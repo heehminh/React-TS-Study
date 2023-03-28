60f35c712b581816c383cf9bf0a2739abe38ade3
heehminh <mink141416@gmail.com>
2023-03-28 16:40:59 +0900

    Typescript 상태관리(3): ReducerSample

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Counter from "./Counter";
+import ReducerSample from "./ReducerSample";
 
 const App: React.FC = () => {
-  return <Counter />;
+  return <ReducerSample />;
 };
 
 export default App;
