520cd0c7efe97bfaab9cb06a5cc205e053cacca4
heehminh <mink141416@gmail.com>
2023-04-07 21:19:45 +0900

    Feat: context

---
+++ react-sidebar-app/src/context/themeContext.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useState, useContext } from "react";
+
+interface ContextProps {
+  theme: string;
+  toggleTheme?: () => void;
+}
+
+export const ThemeContext = createContext<ContextProps>({ theme: "light" });
+
+interface Props {
+  children: any;
+}
+
+export const ThemeContextProvider = ({ children }: Props) => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("useThemeContext must be used within a Theme");
+  }
+
+  return context;
+};
