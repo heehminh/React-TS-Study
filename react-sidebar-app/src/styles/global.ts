c8ec1ea6f3da0837ded61aeac08f4b4c0ac10e54
heehminh <mink141416@gmail.com>
2023-04-07 21:03:15 +0900

    Feat: types, styles

---
+++ react-sidebar-app/src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`    *,
+    *::after,
+    *::before {
+        box-sizing: border-box;
+    }
+    body {
+        background: ${({ theme }) => theme.primary}
+        color: ${({ theme }) => theme.textColor};
+        margin: 0;
+        padding: 0;
+        transition: all 0.25 line-break;
+    }
+`;
