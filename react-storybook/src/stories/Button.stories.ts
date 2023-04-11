169c472f59e6d56751c92a85796aa7e5e23f2722
heehminh <mink141416@gmail.com>
2023-04-11 17:40:43 +0900

    react-weather-app setting (axios)

--- react-storybook/src/stories/Button.stories.ts
+++ react-storybook/src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 // Button.stories.ts|tsx
 
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta } from "@storybook/react";
 
 import { Button } from "./Button";
 
@@ -9,27 +9,8 @@ const meta: Meta<typeof Button> = {
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "Button",
+  title: "Example/But/Button",
   component: Button,
 };
 
 export default meta;
-type Story = StoryObj<typeof Button>;
-
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: "click me",
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: "button2",
-  },
-};
---
+++ react-weather-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const api = {
+  url: process.env.REACT_APP_API_URL,
+  api_key: process.env.RACT_APP_WEATHER_API_KEY,
+};
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
