9738102362243938746ca17d72b203d5631bb74e
heehminh <mink141416@gmail.com>
2023-04-14 01:05:52 +0900

    iphone 계산기 setting

---
+++ react-iphone-calc-clone/src/components/ButtonBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ButtonBox = () => {
+  return <div>ButtonBox</div>;
+};
+
+export default ButtonBox;
---
+++ react-iphone-calc-clone/src/components/Screen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Screen = () => {
+  return <div>Screen</div>;
+};
+
+export default Screen;
